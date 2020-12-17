b13344215964b82ef2a5303add170c0eab5a5a07
Hector Pohle <hectorpohle@gmail.com>
2020-12-17 11:46:17 -0300

    cambios tokens api

--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,33 @@
 const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const Usuario = require('../models/usuario');
 
-passport.use(new localStrategy(
-    function(email, password, done){
-        Usuario.findOne({email: email}, function(err, usuario){
-            if (err) return done(err);
-            if (!usuario) return done(null, false, { message: 'Email no existente o incorrecto'});
-            if (!usuario.validPassword(password)) return done(null, false, {message: 'Password incorrecto'});
-            
-            return done(null, usuario);
-        });
-    },
+
+passport.use(new LocalStrategy(
+    (email, password, done)=>{
+        //we search the user by his/her email
+        Usuario.findOne({email: email}, (err, usuario)=>{
+            //If there is a error
+            if(err) return done(err)
+            //If there is not user with taht email
+            if(!usuario) return done(null, false, {message: 'Email no existente o incorrecto'});
+            //Is password is not valid
+            if (!usuario.validPassword(password)) return done(null, false, {message: 'Password no existente o incorrecto'});
+
+            //Everthing is OK. Execute the callaback
+            return done(null, usuario)
+        })
+    }
 ));
 
-passport.serializeUser(function(usuario, cb){
-    cb(null, usuario.id);
+//cb(callback)
+passport.serializeUser(function(user,cb){
+    cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb){
+passport.deserializeUser((id, cb)=>{
     Usuario.findById(id, function(err, usuario){
-        cb(err, usuario);
+        cb(err, usuario)
     });
 });
 
